b82b75b95deecf50a876b44411f55821f0722f26
Israa1809 <din-israa@hotmail.com>
2022-11-09 10:30:03 +0100

    creates fake cakes

---
+++ src/main/java/dat/backend/model/entities/ShoppingCart.java
@@ -0,0 +1,24 @@
+package dat.backend.model.entities;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+
+    private ArrayList<Cake> cakesInCart;
+
+    public ShoppingCart() {
+        insertCake(new Cake(new Bottom(1, "chocolate", 5), new Topping(9, "blue cheese", 9), 3));
+        insertCake(new Cake(new Bottom(2, "vanilla", 5), new Topping(9, "kiks", 14), 7));
+    }
+
+    public ArrayList<Cake> insertCake(Cake cake) {
+        cakesInCart.add(cake);
+        return cakesInCart;
+    }
+
+    public ArrayList<Cake> removeCake(int cakeId) {
+        cakesInCart.remove(cakeId);
+        return cakesInCart;
+    }
+
+}
